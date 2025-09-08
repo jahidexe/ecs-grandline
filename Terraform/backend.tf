1edb77a4e66ce52a604f9f1d5c8e209584d9891a
jahidexe <46343754+jahidexe@users.noreply.github.com>
2025-09-08 06:42:48 +0100

    fixing typo

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ecs-grand-line-backen"
+    bucket         = "ecs-grand-line-backend"
     key            = "dev/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-locks"
